7e5d1edd9ad0a5357379675c677a6f8b55fc8246
Arthur Luiz <arthurlkingrocha@gmail.com>
2026-09-20 19:10:16 -0300

    Update and rename Tela de Login to Tela de Login.jsx
    Atualização de Tela de Login com Objeto de Estilização

--- Tela de Login.jsx
+++ Tela de Login.jsx
@@ -2,43 +2,26 @@ import {Pressable, Button, StyleSheet, Text, View, Image,TextInput,} from 'react
 
 export default function App() {
   return (
-    <View style={{flex: 1, backgroundColor: '#423338', justifyContent: 'center', alignItems: 'center'}}>
+    <View style={estilo_tela.tela_pricipal}>
       <View>
         <Image
-          source={{
-            uri: 'https://w7.pngwing.com/pngs/573/903/png-transparent-rock-art-cave-painting-petroglyph-cave-fauna-wildlife-hare.png',
-          }}
-          style={{ width: 120, height: 150, margin: 20 }}
+          source={require('./assets/logoddm.png'
+         )}
+          style={estilo_imagem.logo}
         />
       </View>
       <View>
         <Text> Insira seu email abaixo: </Text>
         <TextInput
           palceholder="seu e-mail"
-          style={{
-            borderWidth: 1,
-            borderRadius: 3,
-            margin: 20,
-            padding: 20,
-            color: '#ffffff',
-             backgroundColor: '#ffffff'
-            
-          }}
+          style={estilo_texto.input}
         />  
         
         <Text> Insira sua senha abaixo: </Text>
         <TextInput
           secureTextEntry={true}
           palceholder="seu e-mail"
-          style={{
-            borderWidth: 1,
-            borderRadius: 3,
-            margin: 20,
-            padding: 20,
-            color: '#ffffff',
-            backgroundColor: '#ffff'
-            
-          }}
+          style={estilo_texto.input}
         />
 
         <Button
@@ -66,3 +49,32 @@ export default function App() {
     </View>
   );
 }
+
+
+const estilo_tela =StyleSheet.create ({
+  tela_pricipal:{
+    flex: 1, backgroundColor: '#423338', justifyContent: 'center', alignItems: 'center'
+  }
+})
+
+
+const estilo_texto =StyleSheet.create ({
+  input:{
+            borderWidth: 1,
+            borderRadius: 3,
+            margin: 20,
+            padding: 20,
+            color: '#ffffff',
+            backgroundColor: '#ffff'
+             }
+
+})
+
+
+const estilo_imagem = StyleSheet.create({
+  logo:{
+   width: 160, 
+   height: 170, 
+   margin: 20}
+ 
+});
