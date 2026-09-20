d2a83176896902aeb0125e95860be9dd4eb52e57
Arthur Luiz <arthurlkingrocha@gmail.com>
2026-09-20 19:49:29 -0300

    Tela Home.jsx
    Atualizada com Objetos de Estilização

--- Atividade - Tela Inicial.jsx
+++ Atividade - Tela Inicial.jsx
@@ -1,29 +1,29 @@
-import { Pressable, Button, StyleSheet, Text, View, Image, TextInput, ScrollView, StatusBar} from 'react-native';
+import {
+  Pressable,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  ScrollView,
+  StatusBar,
+} from 'react-native';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <SafeAreaProvider style={{ flex: 1, backgroundColor: '#423338' }}>
-    <StatusBar style="light" />
-      <SafeAreaView style={{ flex: 1 }}>
-        <View
-          style={{
-            width: '100%',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            paddingVertical: 16,
-            borderBottomWidth: 1,
-            borderBottomColor: '#D2B48C',
-            position: 'relative',
-          }}>
+    <SafeAreaProvider style={estilo_tela.safearea1}>
+      <StatusBar style="light" />
+      <SafeAreaView style={estilo_tela.safearea2}>
+        <View style={estilo_tela.tela_pricipal}>
           <Pressable
             onPress={() => {
               alert('Tela de Login!');
             }}>
             <Image
               source={require('./assets/logoddm.png')}
-              style={{ width: 60, height: 60, tintColor: '#D2B48C' }}
+              style={estilo_imagem.imagem_acesa}
             />
           </Pressable>
           <Pressable
@@ -32,7 +32,7 @@ export default function App() {
             }}>
             <Image
               source={require('./assets/home.png')}
-              style={{ width: 60, height: 60 }}
+              style={estilo_imagem.imagem_apagada}
             />
           </Pressable>
 
@@ -42,103 +42,44 @@ export default function App() {
             }}>
             <Image
               source={require('./assets/menu.png')}
-              style={{ width: 60, height: 60, tintColor: '#D2B48C' }}
+              style={estilo_imagem.imagem_acesa}
             />
           </Pressable>
         </View>
 
-        <ScrollView
-          style={{
-            flex: 1,
-            width: '100%',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <Text
-            style={{
-              fontSize: 34,
-              fontWeight: 'bold',
-              color: '#D2B48C',
-              margin: 20,
-              textAlign: 'center',
-            }}>
-            Coleta de vestígios
-          </Text>
+        <ScrollView style={estilo_tela.scroll}>
+          <Text style={estilo_texto.titulo}>Coleta de vestígios</Text>
 
           <View style={{ width: '100%' }}>
-            <Text style={{ color: '#D2B48C', marginBottom: 5 }}>
+            <Text style={estilo_texto.texto_questionario}>
               Insira o nome do seu Sítio Arqueológico:
             </Text>
             <TextInput
               placeholder=" sítio arqueológico"
-              style={{
-                borderWidth: 1,
-                borderRadius: 3,
-                marginBottom: 20,
-                padding: 15,
-                color: '#000000',
-                backgroundColor: '#ffffff',
-              }}
+              style={estilo_texto.input}
             />
 
-            <Text style={{ color: '#D2B48C', marginBottom: 5 }}>
+            <Text style={estilo_texto.texto_questionario}>
               Digite o código do Sítio Arqueológico:
             </Text>
-            <TextInput
-              placeholder="código"
-              style={{
-                borderWidth: 1,
-                borderRadius: 3,
-                marginBottom: 20,
-                padding: 15,
-                color: '#000000',
-                backgroundColor: '#ffffff',
-              }}
-            />
-            <Text style={{ color: '#D2B48C', marginBottom: 5 }}>
+            <TextInput placeholder="código" style={estilo_texto.input} />
+            <Text style={estilo_texto.texto_questionario}>
               Insira o tipo de Vestígio:
             </Text>
             <TextInput
               placeholder=" tipo de vestígio"
-              style={{
-                borderWidth: 1,
-                borderRadius: 3,
-                marginBottom: 20,
-                padding: 15,
-                color: '#000000',
-                backgroundColor: '#ffffff',
-              }}
+              style={estilo_texto.input}
             />
 
-            <Text style={{ color: '#D2B48C', marginBottom: 5 }}>
+            <Text style={estilo_texto.texto_questionario}>
               Descrição sobre o vestígio:
             </Text>
-            <TextInput
-              placeholder="descrição"
-              style={{
-                borderWidth: 1,
-                borderRadius: 3,
-                marginBottom: 20,
-                padding: 15,
-                color: '#000000',
-                backgroundColor: '#ffffff',
-              }}
-            />
-            <Text style={{ color: '#D2B48C', marginBottom: 5 }}>
+            <TextInput placeholder="descrição" style={estilo_texto.input} />
+            <Text style={estilo_texto.texto_questionario}>
               Data e Hora de coleta do vestígio:
             </Text>
-            <TextInput
-              placeholder="data e hora"
-              style={{
-                borderWidth: 1,
-                borderRadius: 3,
-                marginBottom: 20,
-                padding: 15,
-                color: '#000000',
-                backgroundColor: '#ffffff',
-              }}
-            />
-            <Text style={{ color: '#D2B48C', marginBottom: 5 }}>
+            <TextInput placeholder="data e hora" style={estilo_texto.input} />
+            <Text style={estilo_texto.texto_questionario}>
               Insira o arquivo ou mídia do vestígio:
             </Text>
             <Button
@@ -149,7 +90,7 @@ export default function App() {
               }}
             />
 
-            <Text style={{ color: '#D2B48C', marginBottom: 5, marginTop: 20 }}>
+            <Text style={estilo_texto.texto_questionario2}>
               {' '}
               Registre o Vestígio no Inventário:{' '}
             </Text>
@@ -166,7 +107,10 @@ export default function App() {
                 alert('Inventário!');
               }}
               style={{ marginTop: 20, alignItems: 'center' }}>
-              <Text style={{ color: '#D2B48C', fontWeight: 'bold' }}> Ver Inventário</Text>
+              <Text style={{ color: '#D2B48C', fontWeight: 'bold' }}>
+                {' '}
+                Ver Inventário
+              </Text>
             </Pressable>
           </View>
         </ScrollView>
@@ -174,3 +118,74 @@ export default function App() {
     </SafeAreaProvider>
   );
 }
+
+const estilo_tela = StyleSheet.create({
+  safearea1: {
+    flex: 1,
+    backgroundColor: '#423338',
+  },
+
+  safearea2: {
+    flex: 1,
+  },
+
+  tela_pricipal: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#D2B48C',
+    position: 'relative',
+  },
+
+  scroll: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+const estilo_texto = StyleSheet.create({
+  titulo: {
+    fontSize: 34,
+    fontWeight: 'bold',
+    color: '#D2B48C',
+    margin: 20,
+    textAlign: 'center',
+  },
+
+  texto_questionario: {
+    color: '#D2B48C',
+    marginBottom: 5,
+  },
+
+  texto_questionario2: {
+    color: '#D2B48C',
+    marginBottom: 5,
+    marginTop: 20,
+  },
+
+  input: {
+    borderWidth: 1,
+    borderRadius: 3,
+    marginBottom: 20,
+    padding: 15,
+    color: '#000000',
+    backgroundColor: '#ffffff',
+  },
+});
+
+const estilo_imagem = StyleSheet.create({
+  imagem_acesa: {
+    width: 60,
+    height: 60,
+    tintColor: '#D2B48C',
+  },
+  imagem_apagada: {
+    width: 60,
+    height: 60,
+  },
+});
